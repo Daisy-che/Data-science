1acc05261e178f6ad3ccf17c0e8fbe2c59e771b0
Student <daisychepkiruih@gmail.com>
2024-03-31 10:03:26 +0300

    Added a tuple file

---
+++ tuple.py
@@ -0,0 +1,21 @@
+Fruits=('apple','mango','banana','pineapple')
+#assessing the first elemement
+print(Fruits[0])
+
+#assessing the third element
+print(Fruits[2])
+
+#iterate through tuple
+Fruits=('apple','orange','banana')
+for fruit in Fruits:print(Fruits)
+
+#checking if item exist in a tuple
+colors=('Red','pink','blue')
+print('yellow'in colors)
+print('blue'in colors)
+
+#concatenation
+num1=(1,2,3,4,)
+num2=(5,6,8)
+sum=(num1 +num2)
+print(sum)
